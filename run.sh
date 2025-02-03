8a403722f8c09afe1c5463633081d5e25b782c40
tuna2134 <hello@tuna2134.dev>
2025-02-03 08:37:15 +0000

    shell

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 ansible-playbook -i hosts.yml setup_k8s_env.yml
 ansible-playbook -i hosts.yml setup_k8s_master.yml
 
+cp outputs/master_1/etc/kubernetes/admin.conf ~/.kube/config
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
+helm repo add cilium https://helm.cilium.io/
+
+sleep 10
+
+helm install cilium cilium/cilium \
+    --namespace kube-system
